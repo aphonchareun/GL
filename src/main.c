e72edbd2c70be27020147a0acda1c4b8aa2b5f38
antonio daniel <alex.phonchareun@u-psud.fr>
2018-11-14 22:19:39 +0100

    Ajout questions 2 et 3

--- src/main.c
+++ src/main.c
@@ -2,13 +2,19 @@
 //
 
 #include "my_header.h"
+#include <stdlib.h>
+#include<time.h>
 
 int main(void)
 {
     unsigned short s1=0;
     unsigned char n=1;
 	unsigned short s2=0;
-
+	unsigned char nb_rand;
+	unsigned char i=1;
+	unsigned short s3=0;
+	
+	
     while(n<5)
 	{    
     s1=s1+3*n;
@@ -20,6 +26,14 @@ int main(void)
 	s2=s2+3*n;
 	n=n+1;
 	}
-	
+
+	srand(time(NULL)); 
+    while(i<13)
+	{
+	nb_rand =rand()%11;
+	s3=s3+nb_rand;
+	i=i+1;
+	}
+
     return 0;
 }
