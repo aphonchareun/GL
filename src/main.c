eebb39e88dee79cfa2cbb2f82ae559b6203143e2
antonio daniel <alex.phonchareun@u-psud.fr>
2018-11-14 20:51:10 +0100

    Mis a jour pour le 2.

--- src/main.c
+++ src/main.c
@@ -6,12 +6,20 @@
 int main(void)
 {
     unsigned short s1=0;
-    unsigned short n=1;
+    unsigned char n=1;
+	unsigned short s2=0;
 
     while(n<5)
-{    
+	{    
     s1=s1+3*n;
     n=n+1;
-}
+	}
+	
+	while(s2<9873)
+	{
+	s2=s2+3*n;
+	n=n+1;
+	}
+	
     return 0;
 }
