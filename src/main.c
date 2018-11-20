c40441b02590a0c4e37c2fc410fa10257dd25e50
alex phonchareun <alex.phonchareun@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:48:25 +0100

    Rajout du 2

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
-
+#include <ansi_c.h>
 #include "my_header.h"
-
+#include<time.h>
 #define SEUIL 10
+#define SEUILB -100
+#define SEUILH 200
 
 int main(void)
 {
 	unsigned char bin[10] = {1,17,14,16,9,3,7,17,0,12};
-	
-	int i=0;
+	int sat[10] = {rand()%200,rand()%200,rand()%200,rand()%200,rand()%200,rand()%200,rand()%200,rand()%200,rand()%200,rand()%200};
+	unsigned char dist[10] = {1,5,8,10,14,16,30,35,40,50};
+	int i=0, o=0 ;
 	
 	
 	for(i=0; i<10; i++)
@@ -23,6 +26,21 @@ int main(void)
 			bin[i] =1;
 		}
 
+	}
+
+
+	for(o=0; o<10; o++)
+	{
+		srand(time(NULL));
+		if (sat[o] <SEUILB)
+		{	
+			sat[o] =SEUILB;
+		}	
+		else if (sat[o] >SEUILH)
+		{
+			sat[o] =SEUILH;
+		}
+		else sat[o]=sat[o];
 	}
 	
 	
