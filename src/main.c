a686386b9d4f4540181c1d3c7b8393ce1857e846
alex phonchareun <alex.phonchareun@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:31:13 +0100

    je veux mes points

--- src/main.c
+++ src/main.c
@@ -3,7 +3,30 @@
 
 #include "my_header.h"
 
+#define SEUIL 10
+
 int main(void)
 {
-    return 0;
+	unsigned char bin[10] = {1,17,14,16,9,3,7,17,0,12};
+	
+	int i=0;
+	
+	
+	for(i=0; i<10; i++)
+	{
+		if (bin[i] <SEUIL)
+		{	
+			bin[i] =0;
+		}	
+		else
+		{
+			bin[i] =1;
+		}
+
+	}
+	
+	
+	
+	
+	return 0;
 }
