e5aee19a10d909ec88baf9a0d7853205977f5b18
alex phonchareun <alex.phonchareun@ge1.iut-cachan.u-psud.fr>
2018-11-27 11:16:37 +0100

    1/2 de l'exercice

--- src/main.c
+++ src/main.c
@@ -2,8 +2,39 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
 
 int main(void)
 {
+	unsigned char joueur[6] = { 5, 35, 43, 13, 10, 8};
+	unsigned char gagnante[6] = { 5, 14, 43, 13, 15, 1} ;
+	unsigned char nombre_correcte=0;
+	unsigned char i,j;
+	
+	
+	for (i=0;i<6;i++)
+	{
+		for (j=0; j<6;j++)
+		{
+		if (joueur[i]==gagnante[j])
+		{
+			nombre_correcte++;
+		}
+		else 
+		{
+			nombre_correcte = nombre_correcte;
+		}	
+		}	
+	}		
+			
+			
+			
+			
+			
+			
+			
     return 0;
+	
+	
+	
 }
