f8f01c40c2bf07dee367585d919cf2c18e8a4024
alex phonchareun <alex.phonchareun@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:37:56 +0100

    Exercice fini

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+	char nom[100];
+	char prenom[100];
+	unsigned int age;
+	
+	
+	
+	
+	printf("entrer nom:");
+	scanf("%s",nom);
+	
+	printf("entrer prenom:");     
+	scanf("%s",prenom);
+	
+	printf("entrer age:");
+	scanf("%u",&age);
+
+	
+	printf("Je m'appelle %s %s et j'ai %u ans",prenom,nom,age);
+	
+	
     return 0;
 }
